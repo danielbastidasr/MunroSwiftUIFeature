8f9b516e82be6c824354f0bfa91d5627551958cd
danielbastidasr <danielbastidasramirez@gmail.com>
2021-03-21 20:24:14 +0000

    Update MunroFeatureView.swift
    Munro SwiftUI View Public

--- Sources/MunroFilterFeature/presentation/MunroFeatureView.swift
+++ Sources/MunroFilterFeature/presentation/MunroFeatureView.swift
@@ -7,8 +7,10 @@
 
 import SwiftUI
 
-struct MunroFeatureView: View {
-    var body: some View {
+public struct MunroFeatureView: View {
+    public init() {}
+    
+    public var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
