02651cbf0a54dcc92d3681e14d792ea2cbaa4cdd
Daniel Bastidas <danielbastidasramirez@gmail.com>
2021-03-25 17:01:37 +0000

    test navigation

---
+++ Sources/MunroFilterFeature/presentation/MunroResultView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Daniel Bastidas Ramirez on 25/03/2021.
+//
+
+import SwiftUI
+
+struct MunroResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MunroResultView_Previews: PreviewProvider {
+    static var previews: some View {
+        MunroResultView()
+    }
+}
