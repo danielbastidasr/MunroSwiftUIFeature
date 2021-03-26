48acbea02b103a6248242d0fc7e79df9cbd69b4b
Daniel Bastidas <danielbastidasramirez@gmail.com>
2021-03-26 14:20:57 +0000

    Navigation test

--- Sources/MunroFilterFeature/presentation/MunroFeatureView.swift
+++ Sources/MunroFilterFeature/presentation/MunroFeatureView.swift
@@ -11,19 +11,17 @@ public struct MunroFeatureView: View {
     public init() {}
     
     public var body: some View {
-        NavigationView{
-            VStack {
-                Spacer()
-                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                Spacer()
-                
-                NavigationLink(
-                    destination: MunroResultView(),
-                    label: {
-                        /*@START_MENU_TOKEN@*/Text("Navigate")/*@END_MENU_TOKEN@*/
-                    }
-                )
-            }
+        VStack {
+            Spacer()
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Spacer()
+            
+            NavigationLink(
+                destination: MunroResultView(),
+                label: {
+                    /*@START_MENU_TOKEN@*/Text("Navigate")/*@END_MENU_TOKEN@*/
+                }
+            )
         }
     }
 }
